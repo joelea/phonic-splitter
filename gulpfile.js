e430c853fe5627f25dbfd166ff638152f5af16cc
Joe Lea <jlea@palantir.com>
2015-02-21 20:40:57 +0000

    Add gulp watch to the workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,29 @@
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 var mocha = require('gulp-mocha');
+var watch = require('gulp-watch');
 
-var src = 'src/';
+var src = 'src';
 var scripts = src + '/**/*.coffee';
 var test = build + '/test/**/*.js';
 var build = 'build';
 var buildTest = build + '/test/**/*.js';
 
-gulp.task('coffee', function() {
+gulp.task('build', function() {
   return gulp.src(scripts)
              .pipe(coffee())
              .pipe(gulp.dest(build));
 });
 
-gulp.task('test', ['coffee'], function() {
-  gulp.src(buildTest)
-      .pipe(mocha());
+gulp.task('test', ['build'], function() {
+  return gulp.src(buildTest)
+             .pipe(mocha());
 });
 
-gulp.task('default', function() {
-  //
+gulp.task('watch', function() {
+    return watch(scripts, function () {
+        gulp.start('test');
+    });
 });
+
+gulp.task('default', ['build', 'test', 'watch' ], function() {});
